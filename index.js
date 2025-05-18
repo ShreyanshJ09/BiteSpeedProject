751f38a0dbb549da6947bb28f83f61310a316341
ShreyanshJ09 <157059082+ShreyanshJ09@users.noreply.github.com>
2025-05-18 20:40:00 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express, { json } from 'express';
+import cors from 'cors';
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
